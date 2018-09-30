e6e68347f41e9de4ad9583fe76cd37a8572946ab
Tatsiana Korshunava <tachichoco@yahoo.com>
2018-09-30 20:17:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = str;
+  let count = str.length;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets = brackets.split(bracketsConfig[i][0] + bracketsConfig[i][1]).join('');
+    if (brackets.length < count) {
+      count = brackets.length;
+      i = -1;
+    }
+  }
+  return brackets.length === 0 ? true : false;
 }
